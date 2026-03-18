316408bd8462b04da91b282898746993a6e935ff
JPitogo-ph <jpitogodev@gmail.com>
2026-03-18 13:40:46 +0800

    Update entry point files to reflect middleware and new healthcheck route.

---
+++ backend/src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import routes from './routes/index.js';
+
+const app = express();
+
+app.use(helmet);
+//app.use(cors({origin: 'http://localhost:5173'})); //TODO: Configure CORS later
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use('/api', routes)
+
+export default app;
