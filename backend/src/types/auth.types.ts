d44a6de33ddabefc6344dcb70f7264d9db59eb9a
JPitogo-ph <jpitogodev@gmail.com>
2026-03-23 23:53:04 +0800

    Zod schema for updating user data.

--- backend/src/types/auth.types.ts
+++ backend/src/types/auth.types.ts
@@ -1,4 +1,5 @@
 import * as z from 'zod'
+import { Role } from '../../generated/prisma/enums.js';
 
 export const LoginSchema = z.object({
     email: z.email(),
@@ -11,12 +12,21 @@ export const SignupSchema = z.object({
     password: z.string().min(8)
 });
 
+export const UpdateUserSchema = z.object({
+    name: z.string().min(1).optional(),
+    email: z.email(),
+    role: z.enum(Role).optional(),
+}).strict();
+
 export type LoginInput = z.infer<typeof LoginSchema>;
 export type SignupInput = z.infer<typeof SignupSchema>;
+
+export type UpdateUserSchema = z.infer<typeof UpdateUserSchema>;
 
 export interface JwtPayload {
     sub: string;
     email: string;
     role: string;
 }
+
 
