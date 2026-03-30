14943895be559327d5f741d544799ff3a3af31f0
JPitogo-ph <jpitogodev@gmail.com>
2026-03-30 17:10:18 +0800

    Fixed incorrect naming of method and a route.

--- backend/src/routes/ticket.routes.ts
+++ backend/src/routes/ticket.routes.ts
@@ -1,16 +1,16 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/authenticate.middleware.js";
 import { requireRole } from "../middleware/requireRole.middleware.js";
-import { createTicket, getTicket, listTickets, updateticket, deleteTicket } from "../controllers/ticket.controller.js";
+import { createTicket, getTicket, listTickets, updateTicket, deleteTicket } from "../controllers/ticket.controller.js";
 
 const router = Router();
 
 router.use(authenticate);
 
 router.get('/', listTickets);
-router.get(':id', getTicket);
 router.post('/', createTicket);
-router.patch('/:id', updateticket);
+router.get('/:id', getTicket);
+router.patch('/:id', updateTicket);
 router.delete('/:id', requireRole('admin'),deleteTicket)
 
 export default router;
