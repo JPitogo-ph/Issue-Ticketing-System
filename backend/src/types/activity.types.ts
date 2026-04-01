2e38e461afa5bc33ea2d1388531b88781b2416f6
JPitogo-ph <jpitogodev@gmail.com>
2026-04-01 20:14:24 +0800

    Update ActivityMeta to support comment model.

--- backend/src/types/activity.types.ts
+++ backend/src/types/activity.types.ts
@@ -2,7 +2,7 @@ export {};
 
 export type TicketCreatedMeta = {status: 'open'};
 export type TicketUpdatedMeta = {[field: string]: {from: unknown, to: unknown}};
+export type CommentMeta = {commentId: string};
 
-
-export type ActivityMeta = TicketCreatedMeta | TicketUpdatedMeta;
+export type ActivityMeta = TicketCreatedMeta | TicketUpdatedMeta | CommentMeta;
 
